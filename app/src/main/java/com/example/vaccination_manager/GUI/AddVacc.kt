1da1d6fbfc0dc7239c52a2bad0a828c033ad53cf
Nikola Różycka <276075@student.pwr.edu.pl>
2024-04-26 09:26:02 +0200

    first commit

---
+++ app/src/main/java/com/example/vaccination_manager/GUI/AddVacc.kt
@@ -0,0 +1,27 @@
+package com.example.vaccination_manager.GUI
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.vaccination_manager.R
+
+class AddVacc : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_vacc)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun setDate(date: String) {
+        val dateTextView = findViewById<TextView>(R.id.dateText)
+        dateTextView.text = date
+    }
+}
---
+++ app/src/main/java/com/example/vaccination_manager/GUI/CalendarFragment.kt
@@ -0,0 +1,43 @@
+package com.example.vaccination_manager.GUI
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CalendarView
+import androidx.fragment.app.Fragment
+import com.example.vaccination_manager.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CalendarFragment : Fragment() {
+
+    private lateinit var calendarView: CalendarView
+    private lateinit var setDateButton: Button
+    private var selectedDate: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
+        calendarView = view.findViewById(R.id.calendarView)
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val calendar = Calendar.getInstance()
+            calendar.set(year, month, dayOfMonth)
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            selectedDate = dateFormat.format(calendar.time)
+        }
+
+        calendarView.setOnClickListener {
+            val addVacc = activity as? AddVacc
+            addVacc?.setDate(selectedDate ?: "")
+
+            parentFragmentManager.beginTransaction().remove(this).commit()
+        }
+
+        return view
+    }
+}
